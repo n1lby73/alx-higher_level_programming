96ddd1c4142aa3ed44600855601cd85df4369412
n1lby73 <n1lby73@gmail.com>
2023-01-04 04:15:15 -0800

    changed variable name

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if num >= 0:
-    l_digit = num % 10
+if number >= 0:
+    l_digit = number % 10
 else:
-    l_digit = num % -10
+    l_digit = number % -10
 
 print("Last digit of {} is {}".format(num, l_digit), end='')
 
