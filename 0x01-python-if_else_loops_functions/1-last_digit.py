2a35935836764013bc651de8383087b22a05aeca
n1lby73 <n1lby73@gmail.com>
2023-01-04 04:10:40 -0800

    added appropriate conditional statement

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-index = len(str(number))
-print(number[str(index-1)])
+if num >= 0:
+    l_digit = num % 10
+else:
+    l_digit = num % -10
+
+print("Last digit of {} is {}".format(num, l_digit), end='')
+
+if l_digit > 5:
+    print(" and is greater than 5")
+elif l_digit == 0:
+    print(" and is 0")
+else:
+    print(" and is less than 6 and not 0")
